56a3eed66c00132bf6a3be3a94816611e1c5b805
hamid <hamid.bezzout.tech@gmail.com>
2019-06-25 19:00:15 +0100

    modification du fichier main.py (ajout de code calculatrice console

---
+++ main.py
@@ -0,0 +1,17 @@
+a = float(input(" entrer le premier nombre : "))
+b = float(input(" entrer le deuxième nombre : "))
+
+
+add = a + b 
+
+sous = a - b
+
+mult = a * b 
+
+div = a / b
+
+
+print(" "+str(a)+ " + " +str(b)+" = "+ str(add))
+print(" "+str(a)+ " - " +str(b)+" = "+ str(sous))
+print(" "+str(a)+ " * " +str(b)+" = "+ str(mult))
+print(" "+str(a)+ " / " +str(b)+" = "+ str(div))
